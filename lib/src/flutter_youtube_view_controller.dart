c7ea850946cd5b0aff2d680499e0d3e4af9ff7b1
Rodrigo Boratto <rodrigo.rwrz@gmail.com>
2020-07-01 14:05:17 -0300

    Change PlaybackRate to accept double or enum

--- lib/src/flutter_youtube_view_controller.dart
+++ lib/src/flutter_youtube_view_controller.dart
@@ -42,26 +42,23 @@ class FlutterYoutubeViewController {
     await _channel.invokeMethod('seekTo', time);
   }
 
-  Future<void> setPlaybackRate(PlaybackRate rate) async {
-    double rateValue;
+  Future<void> setPlaybackRate({PlaybackRate rate, double rateValue = 1}) async {
+    assert(rate != null || rateValue != null);
     switch (rate) {
-      case(PlaybackRate.RATE_0_25):
+      case PlaybackRate.RATE_0_25:
         rateValue = 0.25;
         break;
-      case(PlaybackRate.RATE_0_5):
+      case PlaybackRate.RATE_0_5:
         rateValue = 0.5;
         break;
-      case(PlaybackRate.RATE_1):
+      case PlaybackRate.RATE_1:
         rateValue = 1;
         break;
-      case(PlaybackRate.RATE_1_5):
+      case PlaybackRate.RATE_1_5:
         rateValue = 1.5;
         break;
-      case(PlaybackRate.RATE_2):
+      case PlaybackRate.RATE_2:
         rateValue = 2.0;
-        break;
-      default:
-        rateValue = 1.0;
         break;
     }
     await _channel.invokeMethod('setPlaybackRate', rateValue);
