cfe2a4b9c144bac168e971f652a00c9cd06c6599
Markus jayjah <jah@jayjah.de>
2022-03-10 13:46:36 +0100

    fix: duplicated method after merge happened

--- android/src/main/kotlin/com/hoanglm/flutteryoutubeview/FlutterYoutubeViewPlugin.kt
+++ android/src/main/kotlin/com/hoanglm/flutteryoutubeview/FlutterYoutubeViewPlugin.kt
@@ -131,10 +131,6 @@ class FlutterYoutubeViewPlugin : FlutterPlugin, ActivityAware, Application.Activ
         }
     }
 
-    override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
-        TODO("Not yet implemented")
-    }
-
     override fun onActivityDestroyed(activity: Activity) {
         if (activity.hashCode() != registrarActivityHashCode) {
             return
